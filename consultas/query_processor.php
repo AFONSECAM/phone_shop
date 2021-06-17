3bd77da0d77790c04aaeead0203b5bd295af2d75
tellejm <61441662+tellejm@users.noreply.github.com>
2021-06-17 15:25:49 -0500

    processor crear

---
+++ consultas/query_processor.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once "includes/class_processor.php"
+$consulta = new processor();
+$consulta_2 =$consulta->query_processor();
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Processor</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+</head>
+<body>
+    <?php
+    for($query=0; $query<sizeof($consulta);$query++){
+        ?>
+        <tr>
+        <td><?php echo $consulta [$query]["processor"]?></td>
+        
+        <td><a href=""></a></td>
+        </tr>      
+        
+        <?php
+    }
+    ?>
+
+
+</body>
+</html>
---
+++ crear/processor.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Crear processor</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+</head>
+
+<body>
+    <?php
+        require_once "../includes/class_processor.php";
+        $ram = new processor();
+        if(empty($_POST['processor'])){
+            echo "<div class='alert alert-danger'>Diligencie el campo processor nombre</div>
+                    <a href='../formularios/form_processor.php' class='btn btn-info'>Intentar nuevamente</a>";
+            return;
+        }else{
+            $ram->crear_processor($_POST['processor']);
+            echo "<div class='alert alert-success'>Se guardó correctamente!</div>
+                    <a href='../formularios/form_processor.php' class='btn btn-info'>Crear nueva ram+</a>";
+        }
+    ?>
+</body>
+
+</html>
